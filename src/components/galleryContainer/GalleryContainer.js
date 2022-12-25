aae3c8f3e1b01d78c84660796a105bc7ebb7b35d
TheTaiMan <68861499+TheTaiMan@users.noreply.github.com>
2022-12-25 14:09:03 -0600

    Added data Page

--- src/components/galleryContainer/GalleryContainer.js
+++ src/components/galleryContainer/GalleryContainer.js
@@ -1,99 +1,6 @@
-import React from 'react';
 import Gallery from '../gallery/Gallery';
 import galleryStyles from './GalleryContainer.module.css';
-
-const data = [
-  {
-    banner: '/banners/anime.jpg',
-    page: {
-      text: 'Anime',
-      icon: '/icons/sowrd.png',
-      size: 17,
-    },
-    tag: 'media',
-  },
-  {
-    banner: '/banners/friends.jpg',
-    page: {
-      text: 'Friends',
-      icon: '/icons/friends.png',
-      size: 17,
-    },
-    tag: 'social',
-  },
-  {
-    banner: '/banners/ajr.jpg',
-    page: {
-      text: 'Music',
-      icon: '/icons/music.png',
-      size: 17,
-    },
-    tag: 'media',
-  },
-  {
-    banner: '/banners/school.jpg',
-    page: {
-      text: 'School',
-      icon: '/icons/school.png',
-      size: 17,
-    },
-    tag: 'social',
-  },
-  {
-    banner: '/banners/canada.png',
-    page: {
-      text: 'Canada',
-      icon: '/icons/canada.png',
-      size: 17,
-    },
-    tag: 'personal',
-  },
-  {
-    banner: '/banners/youtube.jpg',
-    page: {
-      text: 'YouTube',
-      icon: '/icons/youtube.png',
-      size: 17,
-    },
-    tag: 'media',
-  },
-  {
-    banner: '/banners/notion.png',
-    page: {
-      text: 'Notion',
-      icon: '/icons/notion.png',
-      size: 17,
-    },
-    tag: 'media',
-  },
-  {
-    banner: '/banners/space.png',
-    page: {
-      text: 'Astrophysics',
-      icon: '/icons/space.png',
-      size: 17,
-    },
-    tag: 'personal',
-  },
-  {
-    banner: '/banners/coding.png',
-    page: {
-      text: 'Programing',
-      icon: '/icons/coding.png',
-      size: 17,
-    },
-    tag: 'personal',
-  },
-  {
-    banner: '/banners/debating.png',
-    page: {
-      text: 'Debating',
-      icon: '/icons/debating.png',
-      size: 17,
-    },
-    tag: 'personal',
-  },
-];
+import data from '../../data/data';
 
 export default function GalleryContainer() {
   return (
@@ -103,12 +10,12 @@ export default function GalleryContainer() {
           <div className={galleryStyles['container-forth']}>
             <div>
               <div className={galleryStyles['container-fifth']}>
-                {data.map((item) => (
+                {Object.keys(data).map((item) => (
                   <Gallery
-                    banner={item.banner}
-                    page={item.page}
-                    tag={item.tag}
-                    key={item.page.text}
+                    banner={data[item].banner}
+                    page={data[item].page}
+                    tag={data[item].tag}
+                    key={data[item].page.text}
                   />
                 ))}
               </div>
---
+++ src/data/data.js
@@ -0,0 +1,94 @@
+const data = {
+  anime: {
+    banner: '/banners/anime.jpg',
+    page: {
+      text: 'Anime',
+      icon: '/icons/sowrd.png',
+      size: 17,
+    },
+    tag: 'media',
+  },
+  friends: {
+    banner: '/banners/friends.jpg',
+    page: {
+      text: 'Friends',
+      icon: '/icons/friends.png',
+      size: 17,
+    },
+    tag: 'social',
+  },
+  music: {
+    banner: '/banners/ajr.jpg',
+    page: {
+      text: 'Music',
+      icon: '/icons/music.png',
+      size: 17,
+    },
+    tag: 'media',
+  },
+  school: {
+    banner: '/banners/school.jpg',
+    page: {
+      text: 'School',
+      icon: '/icons/school.png',
+      size: 17,
+    },
+    tag: 'social',
+  },
+  canada: {
+    banner: '/banners/canada.png',
+    page: {
+      text: 'Canada',
+      icon: '/icons/canada.png',
+      size: 17,
+    },
+    tag: 'personal',
+  },
+  youtube: {
+    banner: '/banners/youtube.jpg',
+    page: {
+      text: 'YouTube',
+      icon: '/icons/youtube.png',
+      size: 17,
+    },
+    tag: 'media',
+  },
+  notion: {
+    banner: '/banners/notion.png',
+    page: {
+      text: 'Notion',
+      icon: '/icons/notion.png',
+      size: 17,
+    },
+    tag: 'media',
+  },
+  astrophysics: {
+    banner: '/banners/space.png',
+    page: {
+      text: 'Astrophysics',
+      icon: '/icons/space.png',
+      size: 17,
+    },
+    tag: 'personal',
+  },
+  programing: {
+    banner: '/banners/coding.png',
+    page: {
+      text: 'Programing',
+      icon: '/icons/coding.png',
+      size: 17,
+    },
+    tag: 'personal',
+  },
+  debating: {
+    banner: '/banners/debating.png',
+    page: {
+      text: 'Debating',
+      icon: '/icons/debating.png',
+      size: 17,
+    },
+    tag: 'personal',
+  },
+};
+
+export default data;
